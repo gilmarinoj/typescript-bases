61cc404a62b0a0e2a9c60f9d73537cbe13658525
Marino Gil <gilmarinoj@gmail.com>
2024-06-22 16:06:54 -0400

    added objetos docs

---
+++ bases/objetos/objects.ts
@@ -0,0 +1,17 @@
+(() => {
+
+    let flash: { name: string, age: number, powers: string[], getName?: () => string } = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['Super Velocidad', 'Viajar en el tiempo']
+    }
+
+
+    let superman: { name: string, age: number, powers: string[], getName?: () => string } = {
+        name: 'Clark Kent',
+        age: 60,
+        powers: ['Super Velocidad']
+    }
+    
+    
+})()
